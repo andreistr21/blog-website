bfd17caea55e8db8c7502001047c1890e2cd1fe4
andreistr21 <60103919+andreistr21@users.noreply.github.com>
2022-11-21 23:16:42 +0100

    Add post page and serving static files

--- app/views.py
+++ app/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
 
-# Create your views here.
+from app.models import Post
+
+
+def post_page(request, slug):
+    post = Post.objects.get(slug=slug)
+    context = {"post": post}
+
+    return render(request, "app/post.html", context)
+    
--- blogapp/settings.py
+++ blogapp/settings.py
@@ -55,7 +55,7 @@ ROOT_URLCONF = "blogapp.urls"
 TEMPLATES = [
     {
         "BACKEND": "django.template.backends.django.DjangoTemplates",
-        "DIRS": [],
+        "DIRS": [BASE_DIR.joinpath("templates")],
         "APP_DIRS": True,
         "OPTIONS": {
             "context_processors": [
@@ -117,6 +117,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = "static/"
+MEADIA_URL = "/media/"
 MEDIA_ROOT = Path(BASE_DIR).joinpath("upload")
 
 # Default primary key field type
