93ce8f688f18b4d96743e34c2432ec694ff7b18d
andreistr21 <60103919+andreistr21@users.noreply.github.com>
2022-11-22 06:57:39 +0100

    Add tags

--- app/models.py
+++ app/models.py
@@ -1,4 +1,21 @@
+from unicodedata import name
 from django.db import models
+from django.utils.text import slugify
+
+
+class Tag(models.Model):
+    name = models.CharField(max_length=100)
+    description = models.CharField(max_length=100)
+    slug = models.SlugField(max_length=200 ,unique=True)
+    
+    def dave(self, *args, **kwargs):
+        if not self.id:
+            self.slug = slugify(self.name)
+        return super(Tag, self).save(*args, **kwargs)
+    
+    def __str__(self):
+        return self.name
+    
 
 
 class Post(models.Model):
@@ -7,3 +24,8 @@ class Post(models.Model):
     last_updated = models.DateTimeField(auto_now=True)
     slug = models.SlugField(max_length=200, unique=True)
     image = models.ImageField(null=True, blank=True, upload_to="imaages/")
+    
+    # Related name makes it easy to do ?reverse? search
+    # With out a related name: tag_1.post_set.all()
+    # With related nane: tag_1.post.all()
+    tags = models.ManyToManyField(Tag, blank=True, related_name="post")
