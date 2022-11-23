b75eec9c262231cf2d94a10b07f0e048f1cb87b8
andreistr21 <60103919+andreistr21@users.noreply.github.com>
2022-11-23 02:20:38 +0100

    Add top and recent post, tags to the home page

--- app/views.py
+++ app/views.py
@@ -7,8 +7,10 @@ from app.forms import CommentForm
 
 def index(request):
     posts = Post.objects.all()
-    context = {"posts": posts}
-    
+    top_posts = Post.objects.all().order_by("-view_count")[0:3]
+    recent_posts = Post.objects.all().order_by("-last_updated")[0:3]
+    context = {"posts": posts, "top_posts": top_posts, "recent_posts": recent_posts}
+
     return render(request, "app/index.html", context)
 
 
@@ -16,7 +18,7 @@ def post_page(request, slug):
     post = Post.objects.get(slug=slug)
     comments = Comments.objects.filter(post=post, parent=None)
     form = CommentForm()
-    
+
     if request.POST:
         comment_form = CommentForm(request.POST)
         if comment_form.is_valid():
@@ -28,7 +30,7 @@ def post_page(request, slug):
                     comment_reply.post = post
                     comment_reply.save()
                     return redirect("post_page", slug=slug)
-                    
+
             else:
                 comment = comment_form.save(commit=False)
                 post_id = request.POST.get("post_id")
@@ -36,14 +38,13 @@ def post_page(request, slug):
                 comment.post = post
                 comment.save()
                 return redirect("post_page", slug=slug)
-    
+
     if post.view_count is None:
         post.view_count = 1
     else:
         post.view_count += 1
     post.save()
-    
+
     context = {"post": post, "form": form, "comments": comments}
 
     return render(request, "app/post.html", context)
-    
