af8bfab9fada770bf1b4f2c7a9198a2fd5db0c20
andreistr21 <60103919+andreistr21@users.noreply.github.com>
2022-11-28 09:26:16 +0100

    Add profiles

--- app/admin.py
+++ app/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
 
-from app.models import Comments, Post, Tag
+from app.models import Comments, Post, Profile, Tag
 
 admin.site.register(Post)
 admin.site.register(Tag)
 admin.site.register(Comments)
+admin.site.register(Profile)
--- app/models.py
+++ app/models.py
@@ -3,6 +3,21 @@ from django.db import models
 from django.utils.text import slugify
 
 
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    profile_image = models.ImageField(null=True, blank=True, upload_to="images/")
+    slug = models.SlugField(max_length=200, unique=True)
+    bio = models.CharField(max_length=200)
+
+    def save(self, *args, **kwargs):
+        if not self.id:
+            self.slug = slugify(self.user.username)
+        return super(Profile, self).save(*args, **kwargs)
+
+    def __str__(self):
+        return self.user.first_name
+
+
 class Tag(models.Model):
     name = models.CharField(max_length=100)
     description = models.CharField(max_length=100)
@@ -30,6 +45,7 @@ class Post(models.Model):
     # With out a related name: tag_1.post_set.all()
     # With related name: tag_1.post.all()
     tags = models.ManyToManyField(Tag, blank=True, related_name="post")
+    author = models.ForeignKey(User, on_delete=models.CASCADE, null=True, blank=True)
 
 
 class Comments(models.Model):
@@ -38,12 +54,12 @@ class Comments(models.Model):
     name = models.CharField(max_length=200)
     email = models.EmailField(max_length=200)
     website = models.CharField(max_length=200, null=True, blank=True)
-    
+
     post = models.ForeignKey(Post, on_delete=models.CASCADE)
     author = models.ForeignKey(User, on_delete=models.CASCADE, null=True, blank=True)
     parent = models.ForeignKey("self", on_delete=models.DO_NOTHING, null=True, blank=True, related_name="replies")
-    
-    
+
+
 class Subscribe(models.Model):
     email = models.EmailField(max_length=200)
     date = models.DateTimeField(auto_now=True)
