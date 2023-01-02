38ce97eb8a3024b99ac01d534807cc737784beeb
andreistr21 <60103919+andreistr21@users.noreply.github.com>
2023-01-02 14:13:56 +0100

    Fixed tinymce plugin errors

--- blogapp/settings.py
+++ blogapp/settings.py
@@ -102,21 +102,7 @@ AUTH_PASSWORD_VALIDATORS = [
 ]
 
 
-# TINYMCE_DEFAULT_CONFIG = {
-#     "height": "600px",
-#     "width": "960px",
-#     "menubar": "file edit view insert format tools table help",
-#     "plugins": "advlist autolink lists link image charmap print preview anchor searchreplace visualblocks code "
-#     "fullscreen insertdatetime media table paste code help wordcount spellchecker",
-#     "toolbar": "undo redo | bold italic underline strikethrough | fontselect fontsizeselect formatselect | alignleft "
-#     "aligncenter alignright alignjustify | outdent indent |  numlist bullist checklist | forecolor "
-#     "backcolor casechange permanentpen formatpainter removeformat | pagebreak | charmap emoticons | "
-#     "fullscreen  preview save print | insertfile image media pageembed template link anchor codesample | "
-#     "a11ycheck ltr rtl | showcomments addcomment code",
-#     "custom_undo_redo_levels": 10,
-#     "language": "es_ES",  # To force a specific language instead of the Django current language.
-# }
-# TINYMCE_JS_URL = "https://cdn.tiny.cloud/1/jys86spz5w4zp7jbfbr16eorvcw4sx1j2yg6q26namjws4do/tinymce/5/tinymce.min.js"
+TINYMCE_JS_URL = "https://cdn.tiny.cloud/1/jys86spz5w4zp7jbfbr16eorvcw4sx1j2yg6q26namjws4do/tinymce/5/tinymce.min.js"
 TINYMCE_COMPRESSOR = True
 
 
